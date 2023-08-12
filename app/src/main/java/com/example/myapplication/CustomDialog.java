e488f5d56f280465aee8c39b493c3e6da9b70f31
hyowon6 <hyowonng@gmail.com>
2023-08-12 22:13:18 +0900

    [fix] popup

---
+++ app/src/main/java/com/example/myapplication/CustomDialog.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.myapplication.R;
+
+public class CustomDialog extends Dialog {
+
+    private TextView txt_contents;
+    private Button checkPhoneClick;
+    private Button backHome;
+
+
+    @SuppressLint("MissingInflatedId")
+    public CustomDialog(@NonNull Context context, String contents) {
+        super(context);
+        setContentView(R.layout.activity_custom_dialog);
+
+        txt_contents = findViewById(R.id.txt_contents);
+        txt_contents.setText(contents);
+        checkPhoneClick = findViewById(R.id.btn_onsetting);
+        backHome = findViewById(R.id.btn_shutdown);
+
+        checkPhoneClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                Intent intent = new Intent(Settings.ACTION_DISPLAY_SETTINGS);
+                context.startActivity(intent);
+                dismiss();
+            }
+        });
+
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                dismiss();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,104 +1,49 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_dialog;
-    Button btn_dialog1; // 추가된 버튼
-
-    private boolean easyMode = false; // 추가된 변수
+    private Button btn_fontSize;
+    private Button btn_easyMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_dialog = findViewById(R.id.btn_dialog);
-        btn_dialog1 = findViewById(R.id.btn_dialog1); // 버튼 초기화
+        //다이얼로그 밖의 화면은 흐리게 만들어줌
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount = 0.8f;
+        getWindow().setAttributes(layoutParams);
 
-        btn_dialog.setOnClickListener(new View.OnClickListener() {
+        btn_fontSize = findViewById(R.id.btn_fontSize);
+        btn_easyMode = findViewById(R.id.btn_easyMode);
+
+        btn_fontSize.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                showAlertDialog();
+            public void onClick(View view) {
+                showCustomDialog("1. 설정에 들어간다.\n2. '휴대전화 정보'를 클릭한다.");
             }
         });
 
-        btn_dialog1.setOnClickListener(new View.OnClickListener() {
+        btn_easyMode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                showEasyModeDialog();
+            public void onClick(View view) {
+                showCustomDialog("1. 설정에 들어간다.\n2. '쉬운 사용 모드'를 클릭한다.");
             }
         });
+
     }
 
-    private void showAlertDialog() {
-        AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
-        ad.setIcon(R.mipmap.ic_launcher);
-        ad.setTitle("제목");
-        ad.setMessage("메뉴얼");
-
-        ad.setPositiveButton("이동", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                openSettings();
-            }
-        });
-
-        ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        ad.show();
-    }
-
-    private void showEasyModeDialog() {
-        AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
-        ad.setIcon(R.mipmap.ic_launcher);
-        ad.setTitle("제목");
-        ad.setMessage("메뉴얼");
-
-        ad.setPositiveButton("이동", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                openSettings();
-            }
-        });
-
-        ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        ad.show();
-    }
-
-    private void openSettings() {
-        Intent intent = new Intent(Settings.ACTION_DISPLAY_SETTINGS);
-        startActivityForResult(intent, 0);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
-            showToast("설정에서 돌아옴");
-        }
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    private void showCustomDialog(String contents) {
+        CustomDialog customDialog = new CustomDialog(MainActivity.this, contents);
+        customDialog.show();
     }
 }
